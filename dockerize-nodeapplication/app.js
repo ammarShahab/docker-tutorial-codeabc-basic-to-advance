3a0be4f0d97c37e30da0f4e8e3ba6fa93bf65daf
ammarShahab <ashahab007@gmail.com>
2025-12-22 11:13:45 +0600

    commit dockerize-nodejaapplication

---
+++ dockerize-nodeapplication/app.js
@@ -0,0 +1,25 @@
+// 2.0 Dockerize nodejs application
+
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Dockerize The Nodejs application");
+});
+
+// Error route
+app.get("/error", (req, res) => {
+  throw new Error("Something went wrong!");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error Occured: " + err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
