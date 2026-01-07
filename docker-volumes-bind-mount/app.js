12474b7235808331bab80da80526e590046d2e71
Ashahab007 <ashahab007@gmail.com>
2026-01-07 22:40:45 +0600

    updated with video 15

---
+++ docker-volumes-bind-mount/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Dockerize the volume bind mount Nodejs application");
+});
+
+app.get("/error", (req, res) => {
+  throw new Error("Something went wrong!");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error Occurred: " + err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
